4d6cd92290fa0db91bafd8bba6917d8ab2d65a18
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-02-07 04:52:56 +0530

    add room ui done

--- lib/presentation/screens/roomadd/room_add.dart
+++ lib/presentation/screens/roomadd/room_add.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:sumikanova/core/constant/app_color.dart';
+import 'package:sumikanova/core/constant/typography_font.dart';
 import 'package:sumikanova/core/utils/customheader.dart';
 import 'package:sumikanova/core/utils/customtxtformfield.dart';
-import 'package:sumikanova/core/widget/appbutton.dart';
-import 'package:sumikanova/core/widget/custom_iot_type.dart';
-import 'package:sumikanova/core/widget/custom_switch.dart';
-import 'package:sumikanova/core/widget/customback.dart';
-import 'package:sumikanova/core/widget/sunsetcard.dart';
+import 'package:sumikanova/core/utils/reusablemethod.dart';
+import 'package:sumikanova/core/utils/snakbar.dart';
 
 class RoomAddScreen extends StatefulWidget {
   const RoomAddScreen({super.key});
@@ -16,41 +14,121 @@ class RoomAddScreen extends StatefulWidget {
 }
 
 class _RoomAddScreenState extends State<RoomAddScreen> {
+  final TextEditingController roomNameController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  List<Map<String, dynamic>> roomList = [
+    {'id': 1, 'name': 'Living'},
+    {'id': 2, 'name': 'Master'},
+    {'id': 3, 'name': 'Second'},
+    {'id': 4, 'name': 'Dinning'},
+    {'id': 5, 'name': 'Kitchen'},
+    {'id': 6, 'name': 'Study'},
+    {'id': 7, 'name': 'Porch'},
+    {'id': 8, 'name': 'Balcony'},
+    {'id': 9, 'name': 'Kids'},
+    {'id': 10, 'name': 'Closet'},
+    {'id': 11, 'name': 'Master Bedroom'},
+  ];
+
+  @override
+  void dispose() {
+    roomNameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       backgroundColor: AppColor.white2,
-      body: Column(
-        children: <Widget>[
-          CustomHeader(
-            isAllowBack: true,
-            title: 'Add Room',
-            isSubmit: true,
-            onSubmit: () {
-              print('submit');
-            },
-          ),
+      body: Form(
+        key: formKey,
+        child: Column(
+          children: <Widget>[
+            CustomHeader(
+              isAllowBack: true,
+              title: 'Add Room',
+              isSubmit: true,
+              onSubmit: () {
+                final isValid = formKey.currentState!.validate();
+                if (isValid) {
+                  SnakBarUtils.showSnakBar(context, 'Room added successfully');
+                }
+              },
+            ),
 
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-              child: Column(
-                spacing: 20,
-                children: [
-                  CustomTxtFormField(
-                    hintText: 'Room Name',
-                    labelText: 'Room Name',
-                    hintColor: AppColor.black,
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 20,
+                ),
+                child: Column(
+                  spacing: 20,
+                  children: [
+                    CustomTxtFormField(
+                      controller: roomNameController,
+                      hintText: 'Room Name',
+                      labelText: 'Room Name',
+                      hintColor: AppColor.black,
 
-                    validator: (String? value) {
-                      return null;
-                    },
-                  ),
-                ],
+                      validator: (String? value) {
+                        return validationEmpty(value, 'Enter room name');
+                      },
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Recommend',
+                          style: TypographyFont.uih5med.copyWith(
+                            color: AppColor.black,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Wrap(
+                      spacing: 20,
+                      runSpacing: 20,
+                      alignment: WrapAlignment.center,
+                      children: roomList.map<Widget>((
+                        Map<String, dynamic> room,
+                      ) {
+                        return Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 3,
+                          ),
+                          decoration: BoxDecoration(
+                            color: AppColor.white,
+                            borderRadius: BorderRadius.circular(4),
+                            boxShadow: [
+                              BoxShadow(
+                                color: AppColor.gray3.withValues(alpha: 0.3),
+                                blurRadius: 4,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          child: Text(
+                            room['name'] as String,
+                            style: TypographyFont.uih6reg.copyWith(
+                              color: AppColor.black,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
